a03da16ab77d1b5f0ed1ab9e99c4fdd59c43c0f9
Lyle Innes <lyleinnes@Lyles-MacBook-Pro.local>
2017-09-13 16:38:53 +1000

    hacked checkWin, then reduced down, added some styling

--- app.js
+++ app.js
@@ -1,90 +1,70 @@
 console.log("js is on")
 
-var playerOne = "x";
-var playerTwo = "o";
+// --- set important global variables ---
+var playerOne = "X";
+var playerTwo = "O";
 var turn = 0;
-var logic = ["","","","","","","","",""];
 var gameEnd = false;
+var activeBoard = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+var winCombos = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [6, 4, 2]]; 
 var currentPlayer;
 
+// --- referencing the dom elements we need ---
 var board = document.querySelector(".board-wrapper");
-// by adding an event listener on the parent of the divs that represent the game tiles, we can target each div as its clicked (using event targeting)
+var winMessage = document.querySelector(".win-message");
+var resetButton = document.getElementById("reset");
+var meowButton = document.getElementById("meow");
+var children = board.childNodes;
+
+// --- this event listener [which targets whichever tile the player clicks on] is essentially the game ---
 board.addEventListener("click", function(event) {
-  // the first condition sets player as either playerOne or playerTwo depending on the turn variable
+  var index = event.target.dataset.cell;  // each tile has a unique dataset.cell, which we store as a kind of psuedo index..
   if (turn % 2 === 0) {
-    currentPlayer = playerOne
+    currentPlayer = playerOne;
   } else {
-    currentPlayer = playerTwo
+    currentPlayer = playerTwo;
   }
-  // this condition checks that the tile the player clicks is 'inactive' before continuing
-  if (event.target.textContent === "") {
-    // declare an index variable based on the selected tiles data-cell value (these correspond with indexes on the logic array)
-    var index = event.target.dataset.cell;
-    // set the selected tile's text content to the current player's 'marker'
+  if (event.target.textContent === " " && gameEnd === false) {
     event.target.textContent = currentPlayer;
-    // set the corresponding logic array value to player marker by using the data-cell value
-    logic[index] = currentPlayer;
-    // check win here
-    } else {
-    turn += 1;
+    activeBoard[index] = currentPlayer;
+    winCombos.forEach(newWinCheck); // -- peow peow --
+    turn ++;
   }
 });
 
-// --- check win logic ---
+// --- this event listener resets the game ---
+resetButton.addEventListener("click", function() {
+  resetGame();
+})
+
+// --- this event listener executes operation kitty ---
 
 
+// --- newWinCheck function ---
+var newWinCheck = function (array) {
+  if (currentPlayer === "X") {
+    playerNum = "player one"; 
+  } else {
+    playerNum = "player two";
+  }
+  if (activeBoard[array[0]] === activeBoard[array[1]] && activeBoard[array[0]] === activeBoard[array[2]]) {
+    winMessage.textContent = playerNum + " wins";
+    winMessage.className = "transRed";
+    gameEnd = true;
+  } else if (turn === 8 && gameEnd === false) {
+    winMessage.textContent = "draw";
+    winMessage.className = "transRed";
+  }
+};
 
-
-
-
-    // var rowOne = logic[0] + logic[1] + logic[2];
-    // var rowTwo = logic[3] + logic[4] + logic[5];
-    // var rowThree = logic[6] + logic[7] + logic[8];
-    // var colOne = logic[0] + logic[3] + logic[6];
-    // var colTwo = logic[1] + logic[4] + logic[7];
-    // var colThree = logic[2] + logic[5] + logic[8];
-    // var diagTl = logic[0] + logic[4] + logic[8];
-    // var diagBl = logic[6] + logic[4] + logic[2];
-    // if (rowOne === "xxx" || "ooo") {
-    //   console.log('yay');
-    // } else if (rowTwo === "xxx" || "ooo") {
-    //   console.log('yay');
-    // } else if (rowThree === "xxx" || "ooo") {
-    //   console.log('yay');
-    // } else if (colOne === "xxx" || "ooo") {
-    //   console.log('yay');
-    // } else if (colTwo === "xxx" || "ooo") {
-    //   console.log('yay');
-    // } else if (colThree === "xxx" || "ooo") {
-    //   console.log('yay');
-    // } else if (diagTl === "xxx" || "ooo") {
-    //   console.log('yay');
-    // } else if (diagBl === "xxx" || "ooo") {
-    //   console.log('yay');
-    // }
-
-    // if (logic[0] + logic[1] + logic[2] === "xxx" || "ooo") {
-    //   console.log(currentPlayer)
-    // } else if (logic[3] + logic[4] + logic[5] === "xxx" || "ooo") {
-    //   console.log(currentPlayer)
-    // } else if (logic[6] + logic[7] + logic[8] === "xxx" || "ooo") {
-    //   console.log(currentPlayer)
-    // } else if (logic[0] + logic[3] + logic[6] === "xxx" || "ooo") {
-    //   console.log(currentPlayer)
-    // } else if (logic[1] + logic[4] + logic[7] === "xxx" || "ooo") {
-    //   console.log(currentPlayer)
-    // } else if (logic[2] + logic[5] + logic[8] === "xxx" || "ooo") {
-    //   console.log(currentPlayer)
-    // } else if (logic[0] + logic[4] + logic[8] === "xxx" || "ooo") {
-    //   console.log(currentPlayer)
-    // } else if (logic[2] + logic[4] + logic[6] === "xxx" || "ooo") {
-    //   console.log(currentPlayer)
-    // } else {
-
-
-
-
-
-
-
-
+// --- resetGame function ---
+var resetGame = function() {
+  turn = 0;
+  gameEnd = false;
+  activeBoard = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+  children.forEach(function(child){
+    child.textContent = " ";
+  })
+  winMessage.textContent = "";
+  winMessage.className = "win-message";
+}
