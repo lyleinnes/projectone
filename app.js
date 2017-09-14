a02937dd92efad889e029b1644ca7c35b1b25d66
Lyle Innes <lyleinnes@Lyles-MacBook-Pro.local>
2017-09-15 01:48:11 +1000

    re-did all the css, touched up some of the js and html

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 console.log("js is on")
 
 // --- set important global variables ---
-var playerOne = "X";
-var playerTwo = "O";
+var playerOne = "x";
+var playerTwo = "o";
 var turn = 0;
 var gameEnd = false;
 var activeBoard = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
@@ -14,13 +14,13 @@ var board = document.querySelector(".board-wrapper");
 var winMessage = document.querySelector(".win-message");
 var resetButton = document.getElementById("reset");
 var meowButton = document.getElementById("meow");
-var playerOneNum = document.querySelector(".player-one-num");
-var playerTwoNum = document.querySelector(".player-two-num");
+var playerOneChar = document.querySelector(".player-one-char");
+var playerTwoChar = document.querySelector(".player-two-char");
 var children = board.childNodes;
 
-// --- this event listener [which targets whichever tile the player clicks on] is essentially the game ---
+// --- this event listener [which targets whichever tile the player clicks on] executes everything ---
 board.addEventListener("click", function(event) {
-  var index = event.target.dataset.cell;  // each tile has a unique dataset.cell, which we store as a kind of psuedo index..
+  var index = event.target.dataset.cell;  // each tile has a unique dataset.cell, which we use to reference the activeBoard
   if (turn % 2 === 0) {
     currentPlayer = playerOne;
   } else {
@@ -47,23 +47,23 @@ resetButton.addEventListener("click", function() {
 
 // --- newWinCheck function ---
 var newWinCheck = function (array) {
-  if (currentPlayer === "X") {
+  if (currentPlayer === "x") {
     playerNum = "player one"; 
   } else {
     playerNum = "player two";
   }
   if (activeBoard[array[0]] === activeBoard[array[1]] && activeBoard[array[0]] === activeBoard[array[2]]) {
     winMessage.textContent = playerNum + " wins";
-    winMessage.classList.toggle("transRed");
+    winMessage.classList.toggle("unhide");
     gameEnd = true;
-  } 
+  }
 };
 
 // --- nu drawCheck hu dis? ---
 var newDrawAlert = function () {
   if (turn === 8 && gameEnd === false) {
     winMessage.textContent = "draw";
-    winMessage.classList.toggle("transRed");
+    winMessage.classList.toggle("unhide");
     gameEnd = true;
   }
 };
@@ -76,31 +76,30 @@ var resetGame = function() {
   children.forEach(function(child){
     child.textContent = " ";
   });
-  winMessage.classList.toggle("transRed");
-  setTimeout(function() {
-    winMessage.textContent = " ";
-  }, 2000)
   clearInterval(startInt);
   glowOrNoGlow();
+  if (winMessage.classList.value === "win-message unhide") {
+    winMessage.classList.toggle("unhide");
+  }
 };
 
 // --- current player indicator ---
 var glowOrNoGlow = function() {
   if (turn % 2 === 0 && gameEnd === false) {
     startInt = setInterval(function() {
-      playerOneNum.classList.toggle("glowRed")
-    }, 900);
-    playerTwoNum.classList.remove("glowRed")
-  } else if (turn & 2 !== 0 && gameEnd === false) {
+      playerOneChar.classList.toggle("glow") // --- SYNTAX SYNTAX SYNTAX! --- 
+    }, 1000);
+    playerTwoChar.classList.remove("glow");
+  } else if (turn % 2 !== 0 && gameEnd === false) {
     startInt = setInterval(function() {
-      playerTwoNum.classList.toggle("glowRed")
-    }, 900);
-    playerOneNum.classList.remove("glowRed");
+      playerTwoChar.classList.toggle("glow")
+    }, 1000);
+    playerOneChar.classList.remove("glow");
   } else {
-    playerOneNum.classList.remove("glowRed");
-    playerTwoNum.classList.remove("glowRed")
+    playerOneChar.classList.remove("glow");
+    playerTwoChar.classList.remove("glow");
   }
 };
-// --- call it on load ---
+// --- call it on page load ---
 glowOrNoGlow();
 
